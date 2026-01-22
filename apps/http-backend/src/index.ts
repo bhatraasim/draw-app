eed431dd5e90a1ae2f02b5b38c127845605feed1
rasim <rasim44440@gmail.com>
2026-01-22 10:37:07 +0530

    add db prisma

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,61 @@
 import express from "express";
-const app = express()
+import jwt from "jsonwebtoken"
+import { middleware } from "./middleware";
+import { JWT_SECERT } from "@repo/backend-common"
+import { CreateUserSchema  ,  CreateSigninSchema , CreateRoomSchema} from "@repo/common/types"
+
+const app = express();
+app.use(express.json());
 
 
-app.listen(3001);
+
+
+app.post( " signup" , async (req , res) => {
+  
+  const data = CreateUserSchema.safeParse(req.body)
+  if(!data.success){
+     res.json({
+      message : " incorrect inputs"
+    })
+    return
+  }
+
+  
+  // db call  
+
+  res.json({
+    userId : 123
+  })
+})
+
+app.post( "signin" , async (req , res) => {
+  
+  const data = CreateSigninSchema.safeParse(req.body)
+  if(!data.success){
+     res.json({
+      message : " incorrect inputs"
+    })
+    return
+  }
+
+  const userId = 1
+  
+  const token = jwt.sign({
+    userId,
+  },JWT_SECERT)
+})
+
+
+app.post("/room" , middleware , async (req , res) => {
+  const data = CreateRoomSchema.safeParse(req.body)
+  if(!data.success){
+     res.json({
+      message : " incorrect inputs"
+    })
+    return
+  }
+})
+
+app.listen(3001, () => {
+  console.log("HTTP server running on 3001" );
+});
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+import { JWT_SECERT } from "@repo/backend-common"
+
+export function middleware(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization ?? "";
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECERT) as { userId: string };
+
+    req.userId = decoded.userId; // ✅ now VALID
+    next();
+    
+  } catch {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+}
