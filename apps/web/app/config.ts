2ebc8b7063ee54c683b1459c6111662199864d25
rasim <rasim44440@gmail.com>
2026-02-04 11:38:18 +0530

    change harcoded values in config.ts in frontend and ws

--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,2 +1,4 @@
-export const BACKEND_URL = "http://localhost:3001"
-export const WS_URL = "ws://localhost:8080"
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_API_URL ??
+  (process.env.NODE_ENV === "development" ? "http://localhost:3001" : "");
+export const WS_URL = "ws://localhost:8080";
